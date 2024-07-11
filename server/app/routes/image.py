930e145767eb548adcdf2d48478926334686146c
rodyherrera <contact@rodyherrera.com>
2024-07-11 04:53:57 -0400

    chore: now we work with real dynamic images in the database.

--- server/app/routes/image.py
+++ server/app/routes/image.py
@@ -1,20 +1,29 @@
-from fastapi import APIRouter, UploadFile, File
-from PIL import Image
+from fastapi import APIRouter, UploadFile, File, Depends
+from app.db.connection import get_db_dependency
+from motor.motor_asyncio import AsyncIOMotorDatabase
 from app.services.image_processing import compare_images, load_model
+from typing import List
+from PIL import Image
 
 router = APIRouter()
 
 model = load_model()
-database_paths = [
-    '2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg', '7.jpeg', '8.jpeg', '9.jpeg',
-    '10.jpeg', '11.jpeg', '12.jpeg', '13.jpeg', '14.jpeg', '15.jpeg',
-    '16.jpeg', '17.jpeg', '18.jpeg'
-]
+
+async def fetch_objects_samples_urls(db: AsyncIOMotorDatabase) -> List[str]:
+    cursor = db.object.find({ }, { 'samples': 1, '_id': 0 })
+    samples_urls = []
+    async for obj in cursor:
+        samples_urls.extend(obj.get('samples', []))
+    return samples_urls
 
 @router.post('/')
-async def compare_images_endpoint(file: UploadFile = File(...)):
+async def compare_images_endpoint(
+    file: UploadFile = File(...), 
+    db: AsyncIOMotorDatabase = Depends(get_db_dependency)
+):
     query_image = Image.open(file.file)
-    result = compare_images(query_image, database_paths, model)
+    samples = fetch_objects_samples_urls(db)
+    result = compare_images(query_image, samples, model)
     return {
         'status': 'success',
         'data': {
