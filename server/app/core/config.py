c934b9ada9a21d524b00df88b500bd672da99482
Rodolfo Herrera Hernandez <contact@rodyherrera.com>
2024-07-12 07:04:25 +0000

    chore: @server/services/auth.py modularization & performance improvements.

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -6,6 +6,6 @@ class Settings(BaseSettings):
     PUBLIC_DIR: str = 'public'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     DATABASE_URL: str = ''
-    ALLOWED_ORIGINS: list = ['http://172.20.10.2:5173', 'http://localhost:5173']
+    ALLOWED_ORIGINS: list = ['https://172.20.10.2:5173', 'http://localhost:5173']
 
 settings = Settings()
--- server/app/services/auth.py
+++ server/app/services/auth.py
@@ -1,44 +1,48 @@
+from app.schemas.auth import TokenData, AuthResponseModel
 from datetime import datetime, timezone, timedelta
-from jose import JWTError, jwt
+from concurrent.futures import ThreadPoolExecutor
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
-from app.schemas.auth import TokenData, AuthResponseModel
 from app.schemas.user import UserModel
 from app.core.config import settings
-from typing import Optional
 from app.db.connection import get_db
+from jose import JWTError, jwt
+import asyncio
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+executor = ThreadPoolExecutor()
 
-def verify_password(plain_password, hashed_password):
+def verify_password_sync(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+def create_access_token_sync(data: dict):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({'exp': expire})
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({ 'exp': expire })
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
+
+async def verify_password(plain_password, hashed_password):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(executor, verify_password_sync, plain_password, hashed_password)
+
+async def create_access_token(data: dict):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(executor, create_access_token_sync, data)
 
 async def get_user(username: str):
     db = await get_db()
     user = await db.user.find_one({ 'username': username })
-    if user:
-        return user
-    return None
+    return user if user else None
 
 async def authenticate_user(username: str, password: str):
     user = await get_user(username)
-    print(user)
-    if user and verify_password(password, user['hashed_password']):
+    if user and await verify_password(password, user['hashed_password']):
         return user
     return None
 
@@ -50,9 +54,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     )
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
+        username: str = payload.get('sub')
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
@@ -62,7 +64,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     return user
 
 async def create_user_response(username: str, email: str) -> AuthResponseModel:
-    access_token = create_access_token(data={ 'sub': username })
+    access_token = await create_access_token(data={ 'sub': username })
     user_model = UserModel(username=username, email=email)
     return AuthResponseModel(
         status='success',
