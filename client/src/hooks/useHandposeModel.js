185840160317197d30f46a8cdf1777033603f1b2
Rodolfo Herrera Hernandez <contact@rodyherrera.com>
2024-07-17 22:39:45 +0000

    feat: detection if the user's hand is open.

--- client/src/hooks/useHandposeModel.js
+++ client/src/hooks/useHandposeModel.js
@@ -9,11 +9,33 @@ const useHandposeModel = () => {
         handposeModel = await handpose.load();
     };
 
+    const isOpenHand = (landmarks) => {
+        const fingertips = [4, 8, 12, 16, 20];
+        const bases = [0, 5, 9, 13, 17];
+        for(let i = 0; i < fingertips.length; i++){
+            const [tipX, tipY, tipZ] = landmarks[fingertips[i]];
+            const [baseX, baseY, baseZ] = landmarks[bases[i]];
+            if(Math.hypot(tipX - baseX, tipY - baseY, tipZ - baseZ) < 50) return false;
+        }
+        return true;
+    };
+
+    const detectGestures = (predictions) => {
+        for(let i = 0; i < predictions.length; i++){
+            const landmarks = predictions[i].landmarks;
+            if(isOpenHand(landmarks)){
+                console.log('HAND IS OPEN');
+            }else{
+                console.log('NOTHING')
+            }
+        }
+    };
+
     const detectHands = async () => {
         const video = document.querySelector('video');
         const predictions = await handposeModel.estimateHands(video);
         if(predictions.length > 0){
-            console.log(predictions);
+            detectGestures(predictions);
         }
         requestAnimationFrame(detectHands);
     };
