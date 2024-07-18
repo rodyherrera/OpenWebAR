68c9a3c150c0549fb0a945abadce2c0f324b8af2
Rodolfo Herrera Hernandez <contact@rodyherrera.com>
2024-07-18 03:14:57 +0000

    chore: optimization in loading the handpose model and predictions.

--- client/public/handpose-worker.js
+++ client/public/handpose-worker.js
@@ -3,14 +3,36 @@ importScripts('/handpose.min.js');
 
 let model = null;
 
-(async () => {
+const loadModel = async () => {
     console.log('(handpose-worker.js): Loading handpose model...');
-    model = await handpose.load();
+    await tf.setBackend('webgl');
+    model = await handpose.load({
+        maxContinuousChecks: 3,
+        detectionConfidence: 0.7,
+        iouThreshold: 0.2,
+        scoreThreshold: 0.6
+    });
     self.postMessage({ type: 'modelLoaded' });
     console.log('(handpose-worker.js): Model loaded.');
-})();
+};
+
+const resizeImageData = (imageData, newWidth, newHeight) => {
+    const canvas = new OffscreenCanvas(imageData.width, imageData.height);
+    const ctx = canvas.getContext('2d');
+    ctx.putImageData(imageData, 0, 0);
+    const newCanvas = new OffscreenCanvas(newWidth, newHeight);
+    const newCtx = newCanvas.getContext('2d');
+    newCtx.drawImage(canvas, 0, 0, imageData.width, imageData.height, 0, 0, newWidth, newHeight);
+    return newCtx.getImageData(0, 0, newWidth, newHeight);
+};
 
 self.addEventListener('message', async ({ data }) => {
-    const predictions = await model.estimateHands(data.pixels, { flipHorizontal: true });
+    if(!model) return;
+    const newWidth = Math.round(data.pixels.width * 0.5);
+    const newHeight = Math.round(data.pixels.height * 0.5);
+    const resizedImage = resizeImageData(data.pixels, newWidth, newHeight);
+    const predictions = await model.estimateHands(resizedImage, { flipHorizontal: true });
     self.postMessage({ type: 'predictions', predictions });
 });
+
+loadModel();
