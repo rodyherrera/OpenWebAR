4f90d1b887ce2a0de5e1fb1abcb35e0c0e5ed594
Rodolfo Herrera Hernandez <contact@rodyherrera.com>
2024-07-19 00:07:28 +0000

    style: definition of reducers in the "auth" slice.

--- client/src/services/authentication/slice.js
+++ client/src/services/authentication/slice.js
@@ -12,19 +12,19 @@ const authSlice = createSlice({
     name: 'auth',
     initialState: state,
     reducers: {
-        setIsLoading: (state, action) => {
+        setIsLoading(state, action){
             state.isLoading = action.payload;
         },
-        setError: (state, action) => {
+        setError(state, action){
             state.error = action.payload;
         },
-        setIsCachedAuthLoading(state, action) {
+        setIsCachedAuthLoading(state, action){
             state.isCachedAuthLoading = action.payload;
         },
-        setUser: (state, action) => {
+        setUser(state, action){
             state.user = action.payload;
         },
-        setIsAuthenticated: (state, action) => {
+        setIsAuthenticated(state, action){
             state.isAuthenticated = action.payload;
         }
     }
