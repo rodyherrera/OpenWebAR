d6acdb89f5a7977ee5344ffecd71ec713e07def7
Rodolfo Herrera Hernandez <contact@rodyherrera.com>
2024-07-20 09:03:11 +0000

    refactor: creation of the <DashboardHeader /> component

---
+++ client/src/components/dashboard/DashboardHeader/DashboardHeader.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ARPoweredHumans from '@images/dashboard/ar-powered-humans.png';
+import { HiArrowUpRight } from 'react-icons/hi2';
+import './DashboardHeader.css';
+
+const DashboardHeader = () => {
+
+    return (
+        <article className='Dashboard-Header-Container'>
+            <div className='Dashboard-Header-Left-Container'>
+                <div className='Dashboard-Header-Title-Container'>
+                    <p className='Dashboard-Header-Subtitle'>👋 Welcome Rodolfo Herrera!</p>
+                    <h3 className='Dashboard-Header-Title'>What can we do for you today?</h3>
+                </div>
+
+                <article className='Dashboard-Header-Navigation-Container'>
+                    {[
+                        ['Vision Entities', true],
+                        ['My Account'],
+                        ['Settings'],
+                        ['Analytics'],
+                        ['Community']
+                    ].map(([ item, isActive ]) => (
+                        <div className='Dashboard-Header-Navigation-Item-Container' data-isactive={isActive}>
+                            <p className='Dashboard-Header-Navigation-Item-Title'>{item}</p>
+                        </div>
+                    ))}
+                </article>
+            </div>
+            <div className='Dashboard-Header-Right-Container'>
+                <figure className='Brand-News-Container'>
+                    <img 
+                        className='Brand-News-Image'
+                        src={ARPoweredHumans} 
+                        alt='Augmented Reality Powered Humans' />
+                    <figcaption className='Brand-News-Caption-Container'>
+                        <h3 className='Brand-News-Caption-Title'>Start Creating Now</h3>
+                        <i className='Brand-News-Caption-Icon-Container'>
+                            <HiArrowUpRight />
+                        </i>
+                    </figcaption>
+                </figure>
+            </div>
+        </article>
+    );
+};
+
+export default DashboardHeader;
