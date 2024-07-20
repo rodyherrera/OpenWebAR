d52cac1bc8b3e82e102c46afcebfe7fd0a34c1d9
Rodolfo Herrera Hernandez <contact@rodyherrera.com>
2024-07-20 03:25:42 +0000

    feat: tools to facilitate the creation of controllers and API requests.

---
+++ node-server/utilities/runtime.ts
@@ -0,0 +1,33 @@
+import { NextFunction, RequestHandler, Request, Response } from 'express';
+import _ from 'lodash';
+
+export const filterObject = (object: Record<string, any>, ...fields: string[]): Record<string, any> => {
+    return _.pick(object, fields);
+};
+
+export const normalizeUrl = (src: string, baseUrl: string): string | null => {
+    try{
+        if(!src.startsWith('http://') && !src.startsWith('https://')){
+            return new URL(src, baseUrl).href;
+        }
+        return src;
+    }catch(error){
+        return null;
+    }
+};
+
+export const checkIfSlugOrId = (id: string): { _id?: string; slug?: string } => {
+    return /^[a-fA-F0-9]{24}$/.test(id) ? { _id: id } : { slug: id };
+};
+
+export const catchAsync = (
+    asyncFunction: (req: Request, res: Response, next: NextFunction) => Promise<any>
+): RequestHandler => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try{
+            await asyncFunction(req, res, next);
+        }catch (error){
+            next(error);
+        }
+    };
+};
