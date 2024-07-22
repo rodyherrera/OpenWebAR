88fd53ad1c363acb01f2ee0009f580cff3d1c79b
Rodolfo Herrera Hernandez <contact@rodyherrera.com>
2024-07-22 20:21:08 +0000

    refactor: @controllers/ws

--- node-server/controllers/ws.ts
+++ node-server/controllers/ws.ts
@@ -3,6 +3,13 @@ import { ChatResponse } from 'ollama';
 import HandposeDetector from '@services/handposeDetector';
 import Ollama from '@services/ollama';
 
+const initializeOllama = (modelEnvVariable: string, additionalOptions = {}) => {
+    return new Ollama({
+        model: process.env[modelEnvVariable],
+        ...additionalOptions
+    })
+};
+
 const imageHandler = async (blob: string, socket: Socket) => {
     const handposeDetector = new HandposeDetector();
     const tensor = await HandposeDetector.blobToTensor(blob);
@@ -14,51 +21,43 @@ const imageHandler = async (blob: string, socket: Socket) => {
     socket.emit('gesture', gesture);
 };
 
-const ollamaVisionHandler = async (image: string, socket: Socket) => {
-    const ollama = new Ollama({
-        model: process.env.OLLAMA_VISION_MODEL
-    });
-    await ollama.streamResponse('Describe this image', [image], (part: ChatResponse) => {
-        socket.emit('ollama-vision-stream-response', part);
-    });
-};
-
-const ollamaPromptHandler = async (prompt: string, socket: Socket) => {
-    // TODO: fix it, "ana" must be a customizable name,
-    // replace css 'Ana-*' with 'Ollama-*'
-    const ollama = new Ollama({ 
-        model: process.env.OLLAMA_CHAT_MODEL,
-        temperature: 0.7
-    });
-    await ollama.streamResponse(prompt, [], (part: ChatResponse) => {
-        socket.emit('ollama-stream-response', part);
-    });
-};
-
-// REFACTOR THIS
-const ollamaGenerateTitleHandler = async (ctx: string, socket: Socket) => {
-    const ollama = new Ollama({ 
-        model: process.env.OLLAMA_SUMMARIZE_MODEL,
-        temperature: 0.7,
-        maxTokens: 10
-    });
-    const ctxObj = JSON.parse(ctx);
-    const parsedCtx = ctxObj.map(({ content, role }) => `(${role}: ${content})`).join(';');
-    const prompt = `Create a title for the conversation. The title MUST BE EXACTLY 4 WORDS. No more, no less. Context: ${parsedCtx}`;   
-    await ollama.streamResponse(prompt, [], (part: ChatResponse) => {
-        socket.emit('ollama-generate-title-stream-response', part);
+const ollamaHandler = async (action: 'vision' | 'prompt' | 'generate-title', data: string, socket: Socket) => {
+    let prompt = '';
+    let modelEnvVariable = '';
+    let additionalOptions = {};
+    switch(action){
+        case 'vision':
+            prompt = 'Describe this image';
+            modelEnvVariable = 'OLLAMA_VISION_MODEL';
+            break;
+        case 'prompt':
+            prompt = data;
+            modelEnvVariable = 'OLLAMA_CHAT_MODEL';
+            additionalOptions = { temperature: 0.7 };
+            break;
+        case 'generate-title':
+            modelEnvVariable = 'OLLAMA_SUMMARIZE_MODEL';
+            additionalOptions = { temperature: 0.7, maxTokens: 10 };
+            const ctxObj = JSON.parse(data);
+            const parsedCtx = ctxObj.map(({ content, role }) => `(${role}: ${content})`).join(';');
+            prompt = `Create a title for the conversation. The title MUST BE EXACTLY 4 WORDS. No more, no less. Context: ${parsedCtx}`;
+            break;
+    }
+    const ollama = initializeOllama(modelEnvVariable, additionalOptions);
+    await ollama.streamResponse(prompt, action === 'vision' ? [data] : [], (part: ChatResponse) => {
+        const eventName = action === 'vision' ? 'ollama-vision-stream-response' : action === 'prompt' ? 'ollama-stream-response' : 'ollama-generate-title-stream-response';
+        socket.emit(eventName, part);
     });
 };
 
 const connectionHandler = (socket: Socket) => {
     console.log('@controllers/ws: client connected.');
     socket.on('image', (message: string) => imageHandler(message, socket));
-    socket.on('ollama-prompt', (prompt: string) => ollamaPromptHandler(prompt, socket));
-    socket.on('ollama-generate-title', (ctx: string) => ollamaGenerateTitleHandler(ctx, socket))
+    socket.on('ollama-prompt', (prompt: string) => ollamaHandler('prompt', prompt, socket));
+    socket.on('ollama-generate-title', (ctx: string) => ollamaHandler('generateTitle', ctx, socket));
+    socket.on('ollama-vision', (image: string) => ollamaHandler('vision', image, socket));
 };
 
-// todo: the default exported function is the 
-// connection handler for the websocket.
 export default (io: Server) => {
     io.on('connection', connectionHandler);
 };
